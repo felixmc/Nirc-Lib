dcd7fe18811cd836ccfe6622d6e3426aa947b04f
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-01-18 16:02:31 -0700

    fixed bugs with wrong event parameters + small protocol implementations

--- IrcClient.js
+++ IrcClient.js
@@ -9,8 +9,8 @@ function parseUsername(parts) {
 	return parts[0].split("!")[0].slice(1);
 };
 
-function parseRoom(parts) {
-	return parts[2].slice(1);
+function parseRoom(parts, i) {
+	return parts[i || 2].trim().slice(1);
 }
 
 function IrcClient(options) {
@@ -26,35 +26,51 @@ function IrcClient(options) {
 	});
 
 	this.connection.on("data", function(data) {
-		self.emit("data", data);
-		
-		var parts = data.split(" ");
-		
-		console.log(data);
-		
-		if (parts[0] == "PING") {
-			self.pong(parts.slice(1).join(" "));
-		} else if (parts[1] == "JOIN") {
-			self.emit("join", parseRoom(parts), parseUsername(parts));
-		} else if (parts[1] == "QUIT" && parts[1] == "PART") {
-			self.emit("quit", parseRoom(parts), parseUsername(parts));
-		} else if (parts[1] == "332") {
-			self.emit("welcome", parseRoom(parts), data.slice(data.slice(1).indexOf(":") + 2).trim());
-		} else if (parts[1] == "353") {
-			self.emit("names", parseRoom(parts), data.split(":")[2]);
-		} else if (parts[1] == "PRIVMSG") {
-			self.emit("message", parseRoom(parts), parseUsername(parts), data.slice(data.slice(1).indexOf(":") + 2).trim());
-		}
-
+		data.split("\n").forEach(function(dataPart) {
+			if (dataPart != "")
+				self.parseInput(dataPart);
+		});
 	});
 }
 
 util.inherits(IrcClient, EventEmitter);
 
+
+IrcClient.prototype.parseInput = function(data) {
+	var self = this;
+	
+	self.emit("data", data);
+	
+	var parts = data.split(" ");
+
+	console.error(data);
+
+	if (parts[0] == "PING") {
+		self.pong(parts.slice(1).join(" "));
+	} else if (parts[1] == "376") {
+		self.emit("connected");
+	} else if (parts[1] == "JOIN") {
+		self.emit("join", parseRoom(parts).slice(1), parseUsername(parts));
+	} else if (parts[1] == "QUIT") { 
+		self.emit("quit", parseUsername(parts));
+	} else if (parts[1] == "PART") { 
+		self.emit("part", parseRoom(parts), parseUsername(parts));
+	} else if (parts[1] == "332") { // wrong chatroom
+		self.emit("topic", parseRoom(parts, 3), data.slice(data.slice(1).indexOf(":") + 2).trim());
+	} else if (parts[1] == "353") {
+		self.emit("names", parseRoom(parts, 4), data.split(":")[2].trim());
+	} else if (parts[1] == "366") {
+		self.emit("endOfNames", parseRoom(parts, 3));
+	} else if (parts[1] == "PRIVMSG") {
+		self.emit("message", parseRoom(parts), parseUsername(parts), data.slice(data.slice(1).indexOf(":") + 2).trim());
+	} else {
+		console.log("unknown: " + parts);
+	}
+};
+
 IrcClient.prototype.login = function(nick, user, realname) {
 	this.connection.write("NICK " + nick + EOL);
 	this.connection.write("USER " + user + " 8 * :" + realname + EOL);
-	this.emit("connected");
 };
 
 IrcClient.prototype.pong = function(data) {
